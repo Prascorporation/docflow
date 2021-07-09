1d54eec54c3560c4b2d664785acf6b980d5dbaf7
Prascorporation <Artem.prasko10@gmail.com>
2021-07-09 18:56:48 +0300

    first-cards

---
+++ src/cards/cards.js
@@ -0,0 +1,24 @@
+import React from 'react'
+//import SvgLogo from "three_dots_icon_159804.svg";
+//import ReactSVG from 'react-svg';
+import './cards.css'
+import './clear.css'
+{
+    // let date = new Date()
+    // alert(date);
+}
+export default function Cards() {
+    return (
+        <div className="card-style">
+            <div className="second-style"></div>
+            <div className="is-active"> ready</div>
+            <div className="lower-part"></div>
+            <div className="title-name">Title_document.doc</div>
+            <div className="data">20.06.2021</div>
+            <svg width="16px" height="16px" viewBox="0 0 16 16" class="options" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"/>
+</svg>
+        </div>
+
+    )
+}
